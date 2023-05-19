d3fca2b0d1f184f16479cf603a141f4164d904ca
Gabor Raboczki <gabor_raboczky1@epam.com>
2023-05-19 17:46:34 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/unclegabi.github.io",
+  base: "/react-quiz-app",
   plugins: [react()],
 });
